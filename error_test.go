165ae3cee17c5339657390c7aa5f3bda8900270e
Dustin Sallings <dustin@spy.net>
2014-06-06 17:07:44 -0700

    Test for HTTPError
    I thought I had this one, but I just did errorf.  :/

--- error_test.go
+++ error_test.go
@@ -7,6 +7,20 @@ import (
 	"testing"
 )
 
+func TestError(t *testing.T) {
+	res := &http.Response{
+		StatusCode: 404,
+		Status:     "404 Not Found",
+		Body:       ioutil.NopCloser(strings.NewReader("omg, not found")),
+	}
+
+	got := HTTPError(res).Error()
+	exp := `HTTP Error 404 Not Found - omg, not found`
+	if got != exp {
+		t.Errorf("Expected %q, got %q", exp, got)
+	}
+}
+
 func TestErrorf(t *testing.T) {
 	res := &http.Response{
 		StatusCode: 404,
